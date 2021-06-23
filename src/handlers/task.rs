6f9877b26826b6c15f30c2bf595b5cf39d5a794b
Fabien Bellanger <fabien.bellanger@apitic.com>
2021-06-23 14:49:44 +0200

    Stream works. Thanks to Actix community

--- src/handlers/task.rs
+++ src/handlers/task.rs
@@ -36,34 +36,8 @@ pub async fn get_all(pool: web::Data<MySqlPool>) -> Result<impl Responder, AppEr
 }
 
 // Route: GET "/v1/tasks/stream"
-// streaming accept a stream that produce Result<Bytes, impl Into<Error>>.
-// And you have to map your stream to alter the output from Result<Task, sqlx::Error> to it
-// You can use something like async-stream or use futures::stream::StreamExt trait
-// The former one would be easier to work with in async/await
-
-// async fn hi() -> HttpResponse {
-//     let stream = async_stream::stream! {
-//         let rows = vec![1u32, 2, 3];
-//         let mut bytes = BytesMut::with_capacity(64);
-
-//         for row in rows {
-//             let row = format!("row is {}", row);
-//             bytes.extend_from_slice(row.as_bytes());
-
-//             let byte = bytes.split().freeze();
-
-//             yield Ok::<Bytes, Error>(byte)
-//         }
-//     };
-
-//     HttpResponse::Ok().streaming(Box::pin(stream))
-// }
-// Instead of iter a vec you would want to iter your sqlx stream and mapping error and your task type.
-// Since it's derived with serde you must have a format you want for serializing. You can do the serial in the async_stream macro and convert it to a Bytes type.
-
-// Discord link: https://discord.com/channels/771444961383153695/771447545154371646/857221856197083136
 pub async fn get_all_stream(pool: web::Data<MySqlPool>) -> Result<impl Responder, AppError> {
-    let _stream_tasks = async_stream::stream! {
+    let stream_tasks = async_stream::stream! {
         let mut tasks = TaskRepository::get_all(pool.get_ref());
         let mut bytes = BytesMut::with_capacity(64);
 
@@ -71,19 +45,11 @@ pub async fn get_all_stream(pool: web::Data<MySqlPool>) -> Result<impl Responder
             let _task = row.unwrap();
             bytes.extend_from_slice(serde_json::to_string(&_task).unwrap().as_bytes());
             let byte = bytes.split().freeze();
-            yield Ok::<Bytes, sqlx::Error>(byte)
+            yield Ok::<Bytes, AppError>(byte)
         }
     };
 
-    Ok(HttpResponse::Ok().streaming(Box::pin(_stream_tasks)))
-
-    // let stream = crate::models::task::TaskStream {
-    //     number: 100_000, // *number,
-    //     next: 0,
-    //     buf: Default::default(),
-    // };
-
-    // Ok(HttpResponse::Ok().content_type("application/json").streaming(stream))
+    Ok(HttpResponse::Ok().streaming(Box::pin(stream_tasks)))
 }
 
 // Route: GET "/v1/tasks/big"
